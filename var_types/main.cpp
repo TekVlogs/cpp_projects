e7587e3602e5d0893bc96679a44b15ef5a111bf3
abhi <abhijith@c3mdc.com>
2022-06-03 16:18:41 +0530

    type size

---
+++ var_types/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(void)
+{
+	
+	cout << "sizeof(char) : " << sizeof(char) << endl;
+	cout << "sizeof(short int) : " << sizeof(short int) << endl;
+	cout << "sizeof(int) : " << sizeof(int) << endl;
+	cout << "sizeof(long int) : " << sizeof(long int) << endl;
+	cout << "sizeof(long long int) : " << sizeof(long long int) << endl;
+	cout << "sizeof(float) : " << sizeof(float) << endl;
+	cout << "sizeof(double) : " << sizeof(double) << endl;
+	cout << "sizeof(long double) : " << sizeof(long double) << endl;
+
+}
